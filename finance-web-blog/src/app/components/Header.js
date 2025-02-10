c7efde66dbf1c10d60232a9bbd5b087075aa732e
amndzdzdz <amd9105@thi.de>
2025-02-10 11:41:52 +0100

    added some changes to the header

--- finance-web-blog/src/app/components/Header.js
+++ finance-web-blog/src/app/components/Header.js
@@ -1,16 +1,21 @@
 "use client";
-import styles from "./Header.module.css";
+import DropdownMenu from "./Dropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathName = usePathname();
+  var test = `test`;
   return (
     <header style={{ backgroundColor: "#111111" }}>
       <nav className="container">
         <ul className="nav nav-underline mx-auto order-0">
           <li className="nav-item navbar-brand mx-auto">
             <Link
-              className="text-light nav-link active"
+              className={
+                "text-light nav-link " + (pathName === "/" ? "active" : "")
+              }
               aria-current="page"
               href="/"
             >
@@ -18,12 +23,15 @@ export default function Header() {
             </Link>
           </li>
           <li className="nav-item navbar-brand mx-auto">
-            <Link className="text-light nav-link" href="/blogs">
-              Blogs
-            </Link>
+            <DropdownMenu></DropdownMenu>
           </li>
           <li className="nav-item navbar-brand mx-auto">
-            <Link className="text-light nav-link" href="/about">
+            <Link
+              className={
+                "text-light nav-link " + (pathName === "/about" ? "active" : "")
+              }
+              href="/about"
+            >
               About Us
             </Link>
           </li>
