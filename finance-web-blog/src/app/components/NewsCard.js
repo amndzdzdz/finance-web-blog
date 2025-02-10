3e8892e137618308c2db344a324d1387cb4f7bbb
amndzdzdz <amd9105@thi.de>
2025-02-10 11:41:35 +0100

    added home page news cards

--- finance-web-blog/src/app/components/NewsCard.js
+++ finance-web-blog/src/app/components/NewsCard.js
@@ -1,22 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Link from "next/link";
 
 export default function NewsCard() {
   const posts = {
+    id: "1234",
     image: "/images/stock-market.jpg",
     title: "Stock Market Trends for 2025",
     summary:
       "Discover the latest stock market trends and forecasts for the upcoming year.",
     link: "/posts/stock-market-trends-2025",
   };
+  // const router = useRouter();
+  // router.query.NEWPARAMS = posts["id"];
+  // router.push(router);
 
   return (
     <>
-      <div class="card" style={{ width: "18rem" }}>
-        <img class="card-img-top" src="stonks.jpg" alt="Card image cap"></img>
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="stonks.jpg"
+          alt="Card image cap"
+        ></img>
         <hr></hr>
-        <div class="card-body">
-          <h5>Stock Market Trends for 2025</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <Link
+            className="fw-bold fs-6 text-decoration-underline"
+            href="/blogs"
+          >
+            Stock Market Trends for 2025
+          </Link>
+          <p className="card-text">
             Discover the latest stock market trends and forecasts for the
             upcoming year.
           </p>
