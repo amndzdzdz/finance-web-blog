86127d09148986830df1b1e1b89afa631b9bd9c0
amndzdzdz <amd9105@thi.de>
2025-02-10 11:42:55 +0100

    added dropdown menu

---
+++ finance-web-blog/src/app/components/Dropdown.js
@@ -0,0 +1,90 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function DropdownMenu() {
+  const [dropdown, setDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    function handleOutsideClick(event) {
+      if (
+        dropdownRef.current &&
+        buttonRef.current &&
+        !dropdownRef.current.contains(event.target) &&
+        !buttonRef.current.contains(event.target)
+      ) {
+        setDropdown(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
+  let pathName = usePathname();
+
+  return (
+    <div ref={dropdownRef}>
+      <button
+        ref={buttonRef}
+        onClick={() => setDropdown(!dropdown)}
+        className={
+          "text-light nav-link " + (pathName === "/blog-list" ? "active " : " ")
+        }
+        id="dropdownMenuButton"
+        aria-current="page"
+        type="button"
+      >
+        Blogs
+      </button>
+
+      <ul
+        className={`dropdown-menu bg-transparent border-0 ${
+          dropdown ? "show" : ""
+        }`}
+        aria-labelledby="dropdownMenuButton"
+      >
+        <li className="mb-1">
+          <Link
+            className={
+              (pathName !== "/" ? "text-dark" : "text-light") +
+              " text-decoration-underline"
+            }
+            href="/blog-list"
+          >
+            Macroeconomics
+          </Link>
+        </li>
+        <li className="mb-1">
+          <Link
+            className={
+              (pathName !== "/" ? "text-dark" : "text-light") +
+              " text-decoration-underline"
+            }
+            href="/blog-list"
+          >
+            Microeconomics
+          </Link>
+        </li>
+        <li className="mb-1">
+          <Link
+            className={
+              (pathName !== "/" ? "text-dark" : "text-light") +
+              " text-decoration-underline"
+            }
+            href="/blog-list"
+          >
+            Politics
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
