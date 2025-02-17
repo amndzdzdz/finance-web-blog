ea466d38cf22968573dd9140c7ce1bf323fdfbb0
amndzdzdz <zorrofx@gmx.net>
2025-02-17 10:55:21 +0100

    added vertical alternating blog cards

---
+++ finance-web-blog/src/app/components/CardGroup.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./CardGroup.module.css";
+
+const CardGroup = ({ cards }) => {
+  return (
+    <div className="container mt-4">
+      <div className="row">
+        {cards.map((card, index) => (
+          <div
+            key={index}
+            className="col-12 mb-4 shadow-lg p-3 bg-white rounded"
+          >
+            <div className={`card flex-md-row ${styles.customCard}`}>
+              {/* Alternating image position */}
+              {index % 2 === 0 ? (
+                <>
+                  <div className="col-md-4">
+                    <img
+                      className="card-img img-fluid"
+                      src={card.image}
+                      alt="Card"
+                    />
+                  </div>
+                  <div className="col-md-8 card-body">
+                    <h4 className="card-title">{card.title}</h4>
+                    <p className="card-text">{card.text}</p>
+                    <a href={card.link} className="btn btn-outline-primary">
+                      {card.buttonText}
+                    </a>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="col-md-8 card-body">
+                    <h4 className="card-title">{card.title}</h4>
+                    <p className="card-text">{card.text}</p>
+                    <a href={card.link} className="btn btn-outline-primary">
+                      {card.buttonText}
+                    </a>
+                  </div>
+                  <div className="col-md-4">
+                    <img
+                      className="card-img img-fluid"
+                      src={card.image}
+                      alt="Card"
+                    />
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardGroup;
