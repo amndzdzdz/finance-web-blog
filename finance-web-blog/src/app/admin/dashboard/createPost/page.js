c35ae6deda2a484349e0dab7ad65dbc6dd89e226
amndzdzdz <zorrofx@gmx.net>
2025-02-27 12:39:38 +0100

    added thumbnail uploaded using cloudinary

--- finance-web-blog/src/app/admin/dashboard/createPost/page.js
+++ finance-web-blog/src/app/admin/dashboard/createPost/page.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "suneditor/dist/css/suneditor.min.css"; // Import Sun Editor's CSS File
 import { buttonList } from "suneditor-react";
 import { useState } from "react";
+import { CldUploadButton, CldUploadWidget } from "next-cloudinary";
 
 const SunEditor = dynamic(() => import("suneditor-react"), {
   ssr: false,
@@ -12,10 +13,44 @@ const SunEditor = dynamic(() => import("suneditor-react"), {
 
 const MyComponent = (props) => {
   const [editorContent, setEditorContent] = useState("");
+  const [resource, setResource] = useState();
+
+  const [thumbnailUrl, setThumbnailUrl] = useState();
+
+  function handleOnUpload(result, widget, error) {
+    if (error) {
+      return;
+    }
+    setThumbnailUrl(result?.info.secure_url);
+    widget.close({
+      quiet: true,
+    });
+  }
 
   return (
     <div className="m-5">
       <h1 className="text-center mb-5">Create your blog post</h1>
+
+      <div className="mb-3">
+        <CldUploadWidget uploadPreset="ml_default" onSuccess={handleOnUpload}>
+          {({ open }) => {
+            function handleOnClick(e) {
+              e.preventDefault();
+              open();
+            }
+            return (
+              <button
+                type="button"
+                className="btn btn-primary text-white"
+                onClick={handleOnClick}
+              >
+                Upload thumbnail
+              </button>
+            );
+          }}
+        </CldUploadWidget>
+      </div>
+
       <SunEditor
         id="editor"
         name="blog"
@@ -30,12 +65,16 @@ const MyComponent = (props) => {
         }}
       />
 
-      <button
-        onClick={console.log(editorContent)}
-        className="justify-content-center btn btn-outline-primary"
-      >
-        Create Post
-      </button>
+      <div className="text-center mt-5">
+        <button
+          onClick={() => {
+            console.log(thumbnailUrl);
+          }}
+          className="btn btn-outline-primary"
+        >
+          Create Post
+        </button>
+      </div>
     </div>
   );
 };
