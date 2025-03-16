d15e6cc67c68388a4ccdadd697acd559974aac24
amndzdzdz <zorrofx@gmx.net>
2025-03-16 20:01:51 +0100

    changed footer design

--- finance-web-blog/src/app/components/Footer.js
+++ finance-web-blog/src/app/components/Footer.js
@@ -4,28 +4,35 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div
-      style={{ backgroundColor: "#111111", height: "25vh" }}
-      className="footer mt-5 row"
+      style={{ backgroundColor: "#111111" }}
+      className="footer mt-5 py-4 px-3 text-center"
     >
-      <div className="col-5 m-5 text-center">
-        <Link href="/" className="text-light d-block mb-3 ml-3">
-          Home
-        </Link>{" "}
-        <Link href="/blogs" className="text-light d-block mb-3">
-          Blogs
-        </Link>{" "}
-        <Link href="/about" className="text-light d-block">
-          About
-        </Link>{" "}
-      </div>
-      <div className="col-5 mt-5 text-center">
-        <p className="text-light">
-          *name* consists of four students from different universities across
-          Germany, united by our shared passion for economics. Through
-          collaboration and diverse perspectives, we aim to deepen our
-          understanding of economic trends and their impact on businesses and
-          societies worldwide.
-        </p>
+      <div className="container">
+        <div className="row justify-content-center">
+          {/* Navigation Links */}
+          <div className="col-md-5 col-12 mb-4">
+            <Link href="/" className="text-light d-block mb-2">
+              Home
+            </Link>
+            <Link href="/blogs" className="text-light d-block mb-2">
+              Blogs
+            </Link>
+            <Link href="/about" className="text-light d-block">
+              About
+            </Link>
+          </div>
+
+          {/* Description */}
+          <div className="col-md-6 col-12">
+            <p className="text-light">
+              We are four students from different universities across Germany,
+              united by our shared passion for economics. Through collaboration
+              and diverse perspectives, we aim to deepen our understanding of
+              economic trends and their impact on businesses and societies
+              worldwide.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
