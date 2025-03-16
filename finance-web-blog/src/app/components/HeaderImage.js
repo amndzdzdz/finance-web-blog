e627d57ee45f692154d40c8cfbbaa540d63a8cf9
amndzdzdz <zorrofx@gmx.net>
2025-03-16 20:01:08 +0100

    adjusted header design

--- finance-web-blog/src/app/components/HeaderImage.js
+++ finance-web-blog/src/app/components/HeaderImage.js
@@ -1,23 +1,18 @@
+import styles from "./HeaderImage.module.css";
+
 export default function HeaderImage() {
   return (
     <>
       <div
         id="nav-image"
-        className={`bg-image d-flex justify-content-center align-items-center`}
-        style={{
-          width: "100vw",
-          height: "25vh",
-          backgroundImage: 'url("/img.png")',
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
+        className={`bg-image d-flex justify-content-center align-items-center ${styles.headerImage}`}
       >
         <h1 className="text-white">Financial Analysis and News</h1>
       </div>
-      <div className="container-fluid border-top bg-primary">
-        <div className="container text-center text-light w-50 pt-3 pb-3">
-          Get all the latest news, reports and financial Analysis for leading
+
+      <div className="container-fluid border-top bg-primary py-3">
+        <div className="container text-center text-light w-100 w-md-50">
+          Get all the latest news, reports, and financial analysis for leading
           companies around the world!
         </div>
       </div>
